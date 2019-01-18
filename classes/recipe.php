5e9516baab999f16ebdc36baf055293e5105fb91
Nathaniel Gustafson <natjgus@gmail.com>
2019-01-18 18:44:30 +0100

    creating properties and instantianing objects

--- classes/recipe.php
+++ classes/recipe.php
@@ -1,9 +1,30 @@
 <?php
-//Class names should be CamelCased in PHP
+//Class names should be AllCap in PHP
+//Access Modifiers are public, private
 class Recipe
 {
- <I am a code block>
+
+  public $title;
+
+  //Initilizing with an array makes it easier to read and tells
+  public $ingredients = array();
+  public $instructions = array();
+  public $yield;
+  public $tag = array();
+  //Source of all Recipes is Me so I can set dedault
+  public $source = "Nathaniel Gustafson";
+
 }
 
-$recipe1 = new Recipe;
-s
+$recipe1 = new Recipe();
+//Once we create and object we can access it's properties
+//Property name does not start with $ only object name!
+echo $recipe1->source;
+$recipe1->source = "Grandma Natali";
+
+
+$recipe2 = new Recipe();
+$recipe2->source = "Janali";
+
+echo $recipe1->source;
+echo $recipe2->source;
